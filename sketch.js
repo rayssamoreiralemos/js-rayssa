47a909c9727531a3e7a09ab0e1af934b430a2371
rayssamoreiralemos <lemos.rayssa@escola.pr.gov.br>
2024-11-05 14:16:32 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(600, 600);
+    background("black");
+  }
+               
+  function draw() {
+    stroke("blue");
+    fill("red");
+    
+    //console.log(mouseIspressed)
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
+  
